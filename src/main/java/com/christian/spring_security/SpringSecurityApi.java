675261e8bc688238a3fe4337fd0ff6cbab5307c9
ChristianRukundo <siboruremarukundoc1@gmail.com>
2023-09-16 11:15:38 +0200

    Bootstrapped the app

---
+++ src/main/java/com/christian/spring_security/SpringSecurityApi.java
@@ -0,0 +1,16 @@
+package com.christian.spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringSecurityApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApi.class, args);
+    }
+
+
+}
